4aea627ca06d522f830921bc4974859df09b3a44
QuocThang <phanquocthang211005@gmail.com>
2025-04-18 14:11:40 +0700

    update login & sign up

--- Backend/API/api/v1/controller/auth.controller.js
+++ Backend/API/api/v1/controller/auth.controller.js
@@ -1,14 +1,19 @@
-const Account = require('../../../model/account.model') ;
+const Account = require("../../../model/account.model");
 
-module.exports.login = async (req,res) => {
-    const account = await Account.findOne({loginName : req.body.loginName});
-    if (account.password == req.body.password) {
-        res.json({message : "Login Success", token : req.body.token});
-    }
-}
+module.exports.login = async (req, res) => {
+  const account = await Account.findOne({ loginName: req.body.loginName });
+  if (account.password == req.body.password) {
+    res.json({code : 200 , message: "Login Success", token: req.body.token });
+  }
+  else res.json({code : 400 , message : "Invalid Login Name or Password" });
+};
 
-module.exports.signup = async (req,res) => {
-    const account = new Account(req.body) ;
-    await account.save() ;
-    res.json({message : "Sign Up Success"})
-}
+module.exports.signup = async (req, res) => {
+  try {
+    const account = new Account(req.body);
+    await account.save();
+    res.json({code : 200 , message: "Sign Up Success" });
+  } catch {
+    res.json({code : 400 , message: "Sign Up Failed" });
+  }
+};
